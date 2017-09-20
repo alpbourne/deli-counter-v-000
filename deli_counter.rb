777dff385a5ebc78cd59b8fecf0300227767435d
alpbourne <alpbourne@gmail.com>
2017-09-20 00:23:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,9 @@
-def line(array)
-  if array.length == 0
+def line(katz_deli)
+  if katz_deli.length == 0
     puts "The line is currently empty."
-  else array.length > 1
+  else katz_deli.length > 1
     count = []
-    array.each_with_index do |name, index|
+    katz_deli.each_with_index do |name, index|
       count << (" #{index+1}. #{name}")
   end
   puts "The line is currently:" + count.join
