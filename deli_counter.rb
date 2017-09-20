f9dc332791ac371dff2ebcafdadf77a3576748b8
alpbourne <alpbourne@gmail.com>
2017-09-20 00:05:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,10 @@
 def line(array)
-  count = 1
   if array.length == 0
     puts "The line is currently empty."
   else array.length > 1
-
+    count = []
+    array.each_with_index do |name, index|
+      count << ("#{index}. #{name}")
 
 
 def take_a_number
